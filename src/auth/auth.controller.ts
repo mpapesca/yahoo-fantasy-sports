c7cf5fc0904b61fef0d0b07750450951e77918b4
Michael Papesca <michael.papesca@onmail.com>
2022-08-15 21:01:00 -0600

    Add Procfile

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,8 +4,10 @@ import { AuthService } from './auth.service';
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) { }
+
   @Get()
   get(): string {
     return this.authService.get();
   }
+
 }
